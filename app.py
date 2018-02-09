4e41f32ade1305745b65ae91d55a1d332a710237
GUI <guillermo.jury@level3.com>
2018-02-08 23:28:47 -0300

    More samples added

--- app.py
+++ app.py
@@ -91,41 +91,45 @@ def risk(riskid):
     #risk=select((v) for v in Value if v.risk.id==riskid)[:]
     #riskd=to_dict(risk)
     values=db.select("SELECT va.value, ra.type, ra.name, en.value from VALUE va, Risk_attr ra left outer join Enum_value en on en.risk_attrph=ra.id WHERE va.risk_attr=ra.id and va.risk=$riskid ")
-    res={}
-    prev=None
-    for el in values:
-        if(el[0]!=prev):
-            val=[]
-        val.append(el[3])
-        res[el[0]]={'value':el[0],'type':el[1],'name':el[2],'enumv':val[:]}
-        prev=el[0]
-
-    response = Response(json.dumps(list(res.values())),200)
+    response = Response(json.dumps(values),200)
     return response
 
 @app.route('/initdb', methods=["GET"])
 @db_session
 def initdb():
-    rp=Risk(name='Paraglider',description='Need to protect my investment on flying')
-    rb=Risk(name='Boat', description='a lot of money invested on it')
-
-    ra=[0]*5
+    ra=[0]*7
     ra[0]=Risk_attr(name='Address',type='string')
     ra[1]=Risk_attr(name='Valuation', type='number')
-    ra[2]=Risk_attr(name='StealableIndex',type='enum',enum_values=[Enum_value(value='High'),Enum_value(value='Medium'),Enum_value(value='Low')])
-    ra[3]=Risk_attr(name='Purchase',type='date')
+    ra[2]=Risk_attr(name='Stealable Index',type='enum',enum_values=[Enum_value(value='High'),Enum_value(value='Medium'),Enum_value(value='Low')])
+    ra[3]=Risk_attr(name='Purchase Date',type='date')
     ra[4]=Risk_attr(name='Clause',type='string')
-
-    Value(value='5000',risk=rp,risk_attr=ra[1])
+    ra[5]=Risk_attr(name='Date of production',type='date')
+    ra[6]=Risk_attr(name='Driver performance',type='enum',enum_values=[Enum_value(value='Dangerous'),Enum_value(value='Racing fan'),Enum_value(value='Average'),Enum_value(value='Safe')])
+    
+    rp=Risk(name='Paraglider',description='Need to protect my investment on flying')
+    rb=Risk(name='Boat', description='a lot of money invested on it')
+    rc=Risk(name='Car', description='My fast car')
+    
+    Value(value='5000',risk=rp,risk_attr=ra[1]) 
     Value(value='One Way 500',risk=rp,risk_attr=ra[0])
     Value(value='Paragliding is a dangerous thing, please keep it safe',risk=rp,risk_attr=ra[4])
+    
     Value(value='10000000',risk=rb,risk_attr=ra[1])
     Value(value='Bay by the Harbor',risk=rb,risk_attr=ra[0])
     Value(value='High',risk=rb,risk_attr=ra[2])
     Value(value='2018-01-03',risk=rb,risk_attr=ra[3])
     Value(value='Sailing is one of the most wonderful experiences',risk=rb,risk_attr=ra[4])
+
+    Value(value='25000',risk=rc,risk_attr=ra[1])
+    Value(value='Street 12 Dv',risk=rc,risk_attr=ra[0])
+    Value(value='Medium',risk=rc,risk_attr=ra[2])
+    Value(value='2013-07-03',risk=rc,risk_attr=ra[3])
+    Value(value='Sailing is one of the most wonderful experiences',risk=rc,risk_attr=ra[4])
+    Value(value='2005-01-03',risk=rc,risk_attr=ra[5])
+    Value(value='Average',risk=rc,risk_attr=ra[6])
+
     commit()
-    response = Response('OKK', 200)
+    response = Response('OK. All set.', 200)
     return response
 
 
